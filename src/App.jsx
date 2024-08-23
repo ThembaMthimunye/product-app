18378cc31545a41686272347e18b0720846b0d7d
Cassius <111204494+cass-us@users.noreply.github.com>
2024-08-23 13:05:51 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import About from "./components/About"
-
+import About from "./components/About";
+import About from "./components/NavBar.jsx";
 function App() {
 
   return (
     <>
+      <NavBar/>
       <About />
+      
     </>
   )
 }
