34e2d5e40672eacd7a3a1996c6ab2ba9b03675f3
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2024-08-23 14:21:29 +0200

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import About from "./components/About";
 import NavBar from "./components/NavBar.jsx";
 import Testimonies from "./components/Testimonie.jsx";
+import Footer from './components/Footer.jsx';
+import ProductFeature from './components/ProductFeature.jsx'
+
 function App() {
 
   return (
     <>
       <NavBar />
       <Testimonies />
+      <ProductFeature />
       <About />
-
+      <Footer />
     </>
   )
 }
